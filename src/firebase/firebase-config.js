5c8b2f5689e0b1468629464fb9a1707a08d6028e
Tuan Anh <chubatuananh.work@gmail.com>
2024-09-16 23:28:23 +0700

    change enviroment variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCxgFYu_M1sveXomUvsY1X-J05LQFpCYfA',
-  authDomain: 'adventureplan-anhcbt.firebaseapp.com',
-  projectId: 'adventureplan-anhcbt',
-  storageBucket: 'adventureplan-anhcbt.appspot.com',
-  messagingSenderId: '371849693387',
-  appId: '1:371849693387:web:698723c6fa28b89bfcb34e',
-  measurementId: 'G-ZRL7M40EQ8',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
